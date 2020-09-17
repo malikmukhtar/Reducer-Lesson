9e0676fec27ffdb548edcb743dd3bd1ef69da0a5
Malik <malikmukhtar63@gmail.com>
2020-09-17 18:26:47 +0100

    added todo counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { useReducer, useState } from "react";
 function reducer(state, action) {
   if (action.type === "add-todo") {
-    return { todos: [...state.todos, { text: action.text, completed: false }] };
-  } else if (action.type === "decrement") {
-    return state - 1;
+    return {
+      todos: [...state.todos, { text: action.text, completed: false }],
+      todoCount: state.todoCount + 1,
+    };
+  } else if (action.type === "toggle-todo") {
+    return {
+      todos: state.todos.map((todo, idx) =>
+        idx === action.idx ? { ...todo, completed: !todo.completed } : todo
+      ),
+      todoCount: state.todoCount,
+    };
   } else {
     return state;
   }
 }
 
 function App() {
-  const [{ todos }, dispatch] = useReducer(reducer, { todos: [] });
+  const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
+    todos: [],
+    todoCount: 0,
+  });
   const [text, setText] = useState();
   return (
     <div>
@@ -22,8 +33,17 @@ function App() {
         }}
       >
         <input value={text} onChange={(e) => setText(e.target.value)} />
+        <h4>Number of todos: {todoCount}</h4>
       </form>
-      <pre>{JSON.stringify(todos, null, 2)}</pre>
+      {todos.map((todo, idx) => (
+        <h1
+          key={todo.text}
+          onClick={() => dispatch({ type: "toggle-todo", idx })}
+          style={{ textDecoration: todo.completed ? "line-through" : "" }}
+        >
+          {todo.text}
+        </h1>
+      ))}
     </div>
   );
 }
